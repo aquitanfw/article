449038acf951be3bee32cde2c2ff13fe59e6860d
aquitan <aquitan@mail.ru>
2023-03-21 11:55:01 +0300

    article

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $('.popular').slick({
     infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToShow: 3,
+    slidesToScroll: 1,
     prevArrow: `<img src='../../img/arrow-left.svg' class="slick-prev"/>`,
     nextArrow: `<img src='../../img/arrow-right.svg' class="slick-next"/>`,
     responsive: [
@@ -9,7 +9,7 @@ $('.popular').slick({
           breakpoint: 1024,
           settings: {
             slidesToShow: 3,
-            slidesToScroll: 3,
+            slidesToScroll: 1,
             infinite: false,
             arrows: true,
             prevArrow: `<img src='../../img/arrow-left.svg' class="slick-prev"/>`,
@@ -20,7 +20,7 @@ $('.popular').slick({
           breakpoint: 680,
           settings: {
             slidesToShow: 2,
-            slidesToScroll: 2,
+            slidesToScroll: 1,
             arrows: false,
             dots: false,
             arrows: true,
